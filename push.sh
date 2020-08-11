c9fcc8002b2e074e1391aa168dba91c3c5533fb9
zhen <zhen@MacBook-Air-de-zhen.local>
2020-08-11 15:45:40 +0200

    maj

--- push.sh
+++ push.sh
@@ -5,6 +5,10 @@ php bin/console cache:clear
 php bin/console cache:clear --env=prod
 php bin/console cache:clear --env=dev
 
+git add -A 
+git commit -m "maj"
+git push origin master
+
 if [ $1 = "archive" ]
 then
     zip ../symfony-default.zip -r * .[^.]* -x "vendor/*"
