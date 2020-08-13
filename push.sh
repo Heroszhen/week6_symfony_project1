6f18460905e6be0a7d8594ea00a4c81d31419f40
zhen <zhen@MacBook-Air-de-zhen.local>
2020-08-13 17:03:10 +0200

    login,logup,design

--- push.sh
+++ push.sh
@@ -9,10 +9,10 @@ else
     git commit -m "maj"
 fi
 git push origin master
-<<c
+
 php bin/console cache:clear
 php bin/console cache:clear --env=prod
 php bin/console cache:clear --env=dev
-c
+
 
 
